f18351f015fb0e0f3e5af66af86ffca68570c7bf
Tejas Shanbhag <tejasshanbhag14@gmail.com>
2025-02-13 14:19:29 +0530

    final_commit

--- app.py
+++ app.py
@@ -1,3 +1,76 @@
 import streamlit as st
-st.slider()
+from utils import *
+from dotenv import load_dotenv
+from pinecone import Pinecone
+
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+st.set_page_config(layout='wide')
+
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #ADD8E6; /* Light blue */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown("""
+    <style>
+    .stForm {
+        background-color: #ADD8E6;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center;'>Medical Chatbot 💉🚑</h1>", unsafe_allow_html=True)
+st.write(" ")
+st.write(" ")
+
+fig1,fig2 = st.columns(2)
+
+
+with fig1:
+        st.image('media/doctor.jpg',use_container_width=True)
+
+with fig2:
+
+    with st.container(height=750):
+        with st.chat_message("assistant"):
+            st.markdown("Hi I am AI Chatbot!!!")
+                
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+
+    with st.container():
+        human_question =  st.chat_input("What is up?")
+        if human_question:
+
+            embeddings = get_huggingface_embeddings()
+            docstore = get_docstore(collection_name='medical-chatbot',embedding_name=embeddings,api_key=PINECONE_API_KEY)
+            retriever = docstore.as_retriever(search_type='similarity',search_kwargs={"k":5})
+
+            llm = load_gpt_model(openi_api_key=OPENAI_API_KEY)
+            prompt = get_prompt_template()
+            question_answer_chain = create_stuff_documents_chain(llm,prompt)
+            rag_chain = create_retrieval_chain(retriever,question_answer_chain)
+            rag_output = rag_chain.invoke({"input" :str(human_question)})
+            final_answer = rag_output['answer']
+
+            st.session_state.messages.append({"role": "user", "content": human_question})
+
+            st.session_state.messages.append({"role": "assistant", "content": final_answer})
+            st.rerun()
+
 
---
+++ utils.py
@@ -0,0 +1,43 @@
+import numpy as numpy
+import pandas as pd
+import os
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_pinecone import PineconeVectorStore,Pinecone
+from langchain_openai import OpenAI,ChatOpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
+
+def get_huggingface_embeddings():
+    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
+
+def get_docstore(collection_name,embedding_name,api_key):
+    docstore = PineconeVectorStore(index_name=collection_name,embedding=embedding_name,pinecone_api_key=api_key)
+    return docstore
+
+def load_gpt_model(openi_api_key):
+    llm = ChatOpenAI(model="gpt-4o-mini",temperature=0.1,max_tokens=500,api_key=openi_api_key)
+    return llm
+
+def get_prompt_template():
+    system_prompt = (
+                "You are an assistant for question-answering tasks. "
+                "Use the following pieces of retrieved context to answer the question."
+                "If you don't know the answer or any question is out of context just say that you don't know."
+                "Use five sentences maximum and keep the answer concise."
+                "\n\n"
+                "Context:{context}"
+    )
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system",system_prompt),
+            ("human","{input}")
+        ]
+    )
+
+    return prompt
